f22db2577703d864bfd68cce1d37fa7e2e344bb1
Yurii Kotov <y_v_kotov@mail.ru>
2016-09-21 22:21:19 +0300

    done

--- app/js/script.js
+++ app/js/script.js
@@ -3,19 +3,19 @@ window.onload = function anyDoing() {
     function showBannerLogin() {
         var video = document.getElementById("videoPlay");
         var banner = document.getElementById('showBanner');
-        /*video.onclick = function () {
-    if (video.paused) {
-        video.play();
-    } else {
-        video.pause();
-    }
-}*/
+        video.onclick = function () {
+            if (video.paused) {
+                video.play();
+            } else {
+                video.pause();
+            }
+        };
+
         video.addEventListener("canplay", function () {
             setTimeout(function () {
                 video.pause();
-//                video.style.display = "none";
- //                banner.style.display = "block";
-
+                video.style.display = "none";
+                banner.style.display = "block";
             }, 5000);
         });
     };
